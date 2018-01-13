d39744c13dbf912037b7023c042ed69cebf19f63
Nathaniel Hayes <nanobytesinc@gmail.comgit config --global user.email nanobytesinc@gmail.com>
2018-01-13 15:25:14 -0500

    Switched to xmrig for Nicehash support

--- t100ta/mine.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sudo ./minerd -T 60 -R 1 -q -t 4 -a cryptonight -u etnjzeGHJjyCABZWUhaZQp27iGT6xFjmQhRdkKR6PJvCU5wdxMQZiHPP3tUXNDrq56fkbvBTpGkcfWF3k62gBzey8cpg5gqizY.5000@Linaro -p x -o stratum+tcp://pool.etn.spacepools.org:3333
-
-
